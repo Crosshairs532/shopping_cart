e2d203f4b6a9c1847ca3517b2d28297532cd4e78
Md.Samsul Arefin <samsularefin532@gmail.com>
2023-08-21 20:02:39 +0600

    first commit

--- modal.js
+++ modal.js
@@ -2,7 +2,7 @@ document.getElementById('Home').addEventListener('click', function () {
     document.getElementById('total-price').innerText = '00';
     document.getElementById('Discount').innerText = '';
     window.location.href = 'index.html';
-    console.log('fuck you')
+
 
 })
 
